9142a09f5a951fc3307bcc98291bbf8d5baf6d7c
Max Duval <max.duval@theguardian.com>
2023-05-12 16:47:19 +0100

    feat: handle apps and libs

--- src/parse_dependencies.ts
+++ src/parse_dependencies.ts
@@ -7,14 +7,27 @@ export const issues_parser = z.object({
   known_issues: z.record(z.array(z.string())).default({}),
 });
 
-export type Package = z.infer<typeof package_parser>;
-export const package_parser = z.object({
-  name: z.string(),
+const lib = z.object({
+  private: z.literal(false).default(false),
   version: z.string(),
-  private: z.boolean().default(false),
+});
+
+const app = z.object({
+  private: z.literal(true),
+  version: z.string().default("0.0.0"),
+});
+
+const common = z.object({
+  name: z.string(),
   dependencies: dependency.default({}),
   devDependencies: dependency.default({}),
   peerDependencies: dependency.default({}),
   peerDependenciesMeta: z.record(z.object({ optional: z.boolean() }))
     .default({}),
 });
+
+export type Package = z.infer<typeof package_parser>;
+export const package_parser = z.union([
+  lib.merge(common),
+  app.merge(common),
+]);
