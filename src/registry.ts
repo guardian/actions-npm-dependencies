6f45296952e6907a438e56c5263425a493bd7478
Max Duval <max.duval@theguardian.com>
2023-05-16 16:41:32 +0100

    refactor: use JSDelivr for faster responses

--- src/registry.ts
+++ src/registry.ts
@@ -1,7 +1,8 @@
-import { format } from "./colours.ts";
+import { fetchJSON } from "./json.ts";
 import { package_parser } from "./parser.ts";
+import PQueue from "https://deno.land/x/p_queue@1.0.1/mod.ts";
 
-const cache = await caches.open("package-health");
+const queue = new PQueue({ concurrency: 24 });
 
 /** Get package.json of dependencies a given package */
 export const get_registry_dependency = async (
@@ -9,33 +10,19 @@ export const get_registry_dependency = async (
   version: string,
 ) => {
   const url = new URL(
-    `${name}@${version}/package.json`,
-    "https://unpkg.com/",
+    `/npm/${name}@${version}/package.json`,
+    "https://cdn.jsdelivr.net/",
   );
 
-  const interval = setInterval(() => {
-    console.info(
-      `║ Taking a while to fetch information for ${
-        format(name, version)
-      }, ${url}`,
-    );
-  }, 120);
-
-  const found = await cache.match(url);
-  const response = found ?? await fetch(url);
-
-  if (!found) await cache.put(url, response);
-
-  const registry_dependency = await response.clone().json()
-    .then(package_parser.parse);
+  const registry_dependency = await queue.add(() =>
+    fetchJSON(url, { parser: package_parser.parse })
+  );
 
   // We do not want to consider further
   registry_dependency.dependencies = {};
 
   // We do not want to consider development dependencies
   registry_dependency.devDependencies = {};
-
-  clearInterval(interval);
 
   return registry_dependency;
 };
