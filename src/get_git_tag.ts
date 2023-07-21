d88ddda45599a1ed83eaeab38f42e86d34331bda
Max Duval <max.duval@theguardian.com>
2023-07-21 15:33:32 +0100

    feat(dnt): create a Node compatible version
    Co-authored-by: Ara Cho <aracho1@users.noreply.github.com>

---
+++ src/get_git_tag.ts
@@ -0,0 +1,17 @@
+const command = new Deno.Command("git", {
+  args: [
+    "describe",
+    "--tags",
+    "--abbrev=0",
+  ],
+});
+const { code, stdout } = await command.output();
+
+// https://tldp.org/LDP/abs/html/exitcodes.html
+console.assert(code === 0);
+
+const tag = new TextDecoder().decode(stdout);
+
+console.log(`Found tag ${tag}`);
+
+export { tag };
